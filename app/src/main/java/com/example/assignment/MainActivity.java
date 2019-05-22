e202cbb469120ebe0923dfe6bce9027c6910f3cf
Sarah Mukuti <cesarwrites24@gmail.com>
2019-05-22 16:14:45 +0300

    first practice.

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.assignment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.button);
+    }
+
+    public void start(View view) {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
